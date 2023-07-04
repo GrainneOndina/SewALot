34608446b381ab87e52904dab92274aa7b10e47c
LinnSilver <baralinn@gmail.com>
2023-07-04 15:03:27 +0000

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavigationBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <h1>Sewlot</h1>
     </div>
   );
