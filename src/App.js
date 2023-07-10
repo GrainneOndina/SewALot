52c2856c79cd1dc2ba25be4c5251da73be0d8b73
LinnSilver <baralinn@gmail.com>
2023-07-10 18:39:20 +0000

    Add PostEditForm

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import PostCreateForm from "./pages/posts/PostCreateForm";
 
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
-
+import PostEditForm from "./pages/posts/PostEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
@@ -57,7 +57,7 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
 
-
+          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route
             exact
