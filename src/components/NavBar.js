aab28dcb879686f864c0f8418551078842fa0c93
LinnSilver <baralinn@gmail.com>
2024-04-09 10:06:43 +0000

    Add style and arialables to navbar. Add new logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,24 +44,11 @@ const NavBar = () => {
 
   const loggedInIcons = (
     <>
-      <NavLink
-        className={styles.NavLink}
-        activeClassName={styles.Active}
-        exact
-        to="/"
+      <NavLink className={styles.NavLink} 
+        to="/" 
+        onClick={handleSignOut}
+        aria-label="Sign Out"
       >
-        <i className="fas fa-home"></i>Home
-      </NavLink>
-
-      <NavLink
-        className={styles.NavLink}
-        activeClassName={styles.Active}
-        to="/liked"
-      >
-        <i className="fas fa-heart"></i>Liked
-      </NavLink>
-      
-      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
         <i className="fas fa-sign-out-alt"></i>Sign out
       </NavLink>
       
@@ -69,6 +56,7 @@ const NavBar = () => {
         className={`${styles.NavLink} ${styles.AvatarLink}`}
         activeClassName={styles.activeAvatar}
         to={`/profiles/${currentUser?.profile_id}`}
+        aria-label="Profile page"
       >
         <img
           className={styles.AvatarImage}
@@ -85,6 +73,7 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
         to="/signin"
+        aria-label="Sign In"
       >
         <i className="fas fa-sign-in-alt"></i>Sign in
       </NavLink>
@@ -92,6 +81,7 @@ const NavBar = () => {
         to="/signup"
         className={styles.NavLink}
         activeClassName={styles.Active}
+        aria-label="Sign Up"
       >
         <i className="fas fa-user-plus"></i>Sign up
       </NavLink>
@@ -99,24 +89,54 @@ const NavBar = () => {
   );
 
   return (
-    <div className="container">
-      <Navbar
-        key={currentUser ? currentUser.id : "guest"}
-        expanded={expanded}
-        className={styles.NavBar}
-        expand="md"
-        fixed="top"
-      >
-        <div className="container">
+    <div className="col-lg-8">
+        <Navbar
+          key={currentUser ? currentUser.id : "guest"}
+          expanded={expanded}
+          className={styles.NavBar}
+          expand="md"
+          fixed="top"
+          style={{ justifyContent: "space-between" }}
+        >
+        <div>
           <NavLink to="/">
-            <Navbar.Brand>
-              <img src={logo} alt="logo" className={styles.Logo} />
-            </Navbar.Brand>
+            <img src={logo} 
+            alt="logo" 
+            className={styles.Logo} 
+            aria-label="Logo / Home"
+            />
           </NavLink>
+        </div>
+
+         <div>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            exact
+            to="/"
+            aria-label="Home"
+          >
+            <i className="fas fa-home"></i>Home
+          </NavLink>
+        </div>
+
+        <div>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/liked"
+            aria-label="Liked"
+          >
+            <i className="fas fa-heart"></i>Liked
+          </NavLink>
+        </div>
+
+        <div>
           <Navbar.Toggle
             ref={ref}
             onClick={() => setExpanded(!expanded)}
             aria-controls="basic-navbar-nav"
+            aria-label="Menu navigation"
           />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
@@ -124,6 +144,7 @@ const NavBar = () => {
             </Nav>
           </Navbar.Collapse>
         </div>
+
       </Navbar>
     </div>
   );
